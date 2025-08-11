387f2c432d28bd3520b313c7d620dc1e27af126f
nguyentris2511 <86146293+nguyentris2511@users.noreply.github.com>
2025-08-11 17:12:34 +0700

    commit

--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ export default function handler(req, res) {
                 }
             ]
         },
+        "icon": "https://static.momocdn.net/app/img/icon_new3/expense.png",
+        "title": "Cửa hàng của bạn",
         "itemData": [
             {
                 "itemId": "",
